3d44060df64c03df7a0ae50fbcfdf5c1990759b4
Gustavo Henrique Spiess <gustavo.spiess@philips.com>
2020-10-13 21:07:44 -0300

    form post

--- CODIGO-FONTE/interface/snetgen/core/views.py
+++ CODIGO-FONTE/interface/snetgen/core/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import FormView, TemplateView
 from django.urls import reverse_lazy
 
+import csv
+
 from .forms import ConfigForm
 
 
@@ -8,6 +10,19 @@ class ConfigFormView(FormView):
     form_class = ConfigForm
     success_url = reverse_lazy('success')
     template_name = 'crispy_form.html'
+
+    def form_valid(self, form):
+
+        data = form.cleaned_data
+
+        hash_id = abs(hash(tuple(data[k] for k in sorted(data.keys()))))
+
+        with open(str( hash_id ), 'w', newline='') as data_buffer:
+            file_writer = csv.writer(data_buffer)
+            file_writer.writerow(data[k] for k in sorted(data.keys()))
+            file_writer.writerow(k for k in sorted(data.keys()))
+
+        return super().form_valid(form)
 
 
 class SuccessView(TemplateView):
--- CODIGO-FONTE/interface/snetgen/urls.py
+++ CODIGO-FONTE/interface/snetgen/urls.py
@@ -1,11 +1,11 @@
+from django.http import HttpResponseRedirect
 from django.urls import path
 from django.views.generic import RedirectView
 
 from snetgen.core import views
 
-
 urlpatterns = [
     path('', views.ConfigFormView.as_view(), name='config_form'),
-    path('config', views.ConfigFormView.as_view(), name='config_form'),
+    path('config/', views.ConfigFormView.as_view(), name='config_form'),
     path('success/', views.SuccessView.as_view(), name='success'),
 ]
